71de2955ca78c4a00ff1e4f0c0709451a2acb633
FranklinPTorres <105072367+FranklinPTorres@users.noreply.github.com>
2022-10-20 14:38:55 +0100

    update

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-//You can edit ALL of the code here
-function setup() {
+  const rootElem = document.getElementById("root");
   const allEpisodes = getAllEpisodes();
+  function setup() {
+  
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+ 
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach(element => {
+    const container = document.createElement('div')
+    container.className = 'GOTEps'
+     const container2 = document.createElement('header')
+      const container3 = document.createElement('img')
+       const container4 = document.createElement('p')
+       container2.innerText = `${element.name} - S0${element.season}E${
+      element.number < 10 ? "0" + element.number : element.number
+    }`
+       container3.src = element.image.medium
+       container4.innerHTML = element.summary
+       container.append(container2,container3,container4);
+       rootElem.append(container)
+  });
 }
+let searchEps = (episodes) => {
+  let searchBar = document.getElementById("search");
+  searchBar.addEventListener("input", (e) => {
+   let userInput = e.target.value.toLowerCase()
+   let filerEps = episodes.filter((e) =>{
+    return e.name.toLowerCase().includes(userInput) || e.summary.toLowerCase().includes(userInput)
+   });
+   rootElem.innerHTML = '';
+   makePageForEpisodes(filerEps);
+  });
+}
+searchEps(allEpisodes)
+
+// 100 done, 200 done,
 
 window.onload = setup;
+
